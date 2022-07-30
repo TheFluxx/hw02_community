2b143a3531b0e116cf7bdf22c7eac03cfcb82455
Платон <zhemerov17@mail.ru>
2022-07-30 15:22:32 +0300

    commit for revue

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -8,5 +8,3 @@ urlpatterns = [
     path('', views.index, name='posts'),
     path('group/<slug:slug>', views.group_posts, name='group_list'),
 ]
-
-
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
+
 def index(request):
     title = 'Последние обновления на сайте'
     posts = Post.objects.order_by('-pub_date')[:10]
@@ -10,6 +11,7 @@ def index(request):
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
+
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
@@ -21,3 +23,4 @@ def group_posts(request, slug):
         'title': title,
     }
     return render(request, 'posts/group_list.html', context)
+    
